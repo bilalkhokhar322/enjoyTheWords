f79a7d840db6c062a0399ba0bc84596320970da7
bilalkhokhar322 <muhammad.bilal@argonteq.com>
2024-05-31 19:03:36 +0500

    add pagination

--- src/Pages/User/Index.jsx
+++ src/Pages/User/Index.jsx
@@ -4,13 +4,16 @@ import { edit, del } from "../../Assets/icons/index";
 import UserTable from "../../Shared/UserTable/Index";
 import HeadingText from "../../Shared/MainHeading/Index";
 import { deleteUser, userListing } from "../../Redux/features/User/userApi";
+import ReactPaginate from "react-paginate";
+import { PER_PAGE_SIZE } from "../../utils/constants";
 
 const User = () => {
   const dispatch = useDispatch();
   const tableHeaderData = ["#", "Name", "Number", "Email", "Status", "Actions"];
-
   const [usersData, setUsersData] = useState([]);
-  const [tableData, setTableData] = useState("");
+  const [tableData, setTableData] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     fetchUsers();
@@ -63,7 +66,6 @@ const User = () => {
     );
   }, [usersData]);
 
-  console.log(usersData);
   const handleDelete = (userId) => {
     const user = {
       apiEndpoint: `/admin/deleteUser?userId=${userId}`,
@@ -76,21 +78,39 @@ const User = () => {
   };
 
   const fetchUsers = () => {
+    const pageNumber = currentPage + 1;
+
     const data = {
-      apiEndpoint: `/admin/listUser?page=${1}&size=${10}`,
+      apiEndpoint: `/admin/listUser?page=${pageNumber}&size=${PER_PAGE_SIZE}`,
     };
     dispatch(userListing(data)).then((res) => {
       if (res.type === "userListing/fulfilled") {
-        // setUsersData(res);
         setUsersData(res?.payload?.data?.users);
+        setPageCount(res?.payload?.data?.totalUser / PER_PAGE_SIZE);
       }
     });
+  };
+
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected;
+    setCurrentPage(selectedPage);
   };
 
   return (
     <>
       <HeadingText className={"text-white display-5"} Text={"Users"} />
       <UserTable headerData={tableHeaderData} bodyData={tableData} />
+
+      {usersData.length && (
+        <ReactPaginate
+          pageCount={Math.ceil(pageCount)} // Total number of pages
+          pageRangeDisplayed={3} // Number of page links to display
+          marginPagesDisplayed={3} // Number of pages to display at the margins
+          onPageChange={handlePageClick} // Function to handle page click
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+        />
+      )}
     </>
   );
 };
--- src/Pages/usersLogs/Index.jsx
+++ src/Pages/usersLogs/Index.jsx
@@ -4,6 +4,8 @@ import UserTable from "../../Shared/UserTable/Index";
 import { useDispatch } from "react-redux";
 import { userLogs } from "../../Redux/features/User/userApi";
 import moment from "moment";
+import ReactPaginate from "react-paginate";
+import { PER_PAGE_SIZE } from "../../utils/constants";
 
 const UsersLogs = () => {
   const tableHeadingArr = [
@@ -18,70 +20,30 @@ const UsersLogs = () => {
   ];
   const dispatch = useDispatch();
   const [logsData, setLogsData] = useState([]);
-  const [tableData, setTableData] = useState("");
+  const [tableData, setTableData] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     fetchLogs();
-  }, [dispatch]);
-
-  console.log(logsData);
+  }, [dispatch, currentPage]);
 
   useEffect(() => {
     setTableData(
-      <>
-        {logsData?.map((data) => (
-          <tr className="">
-            <td
-              style={{
-                fontSize: "13px",
-              }}
-            >
-              {data?.userName}
-            </td>
-            <td
-              style={{
-                fontSize: "13px",
-              }}
-            >
-              {data?.method}
-            </td>
-            <td
-              style={{
-                fontSize: "13px",
-                width: "10px",
-              }}
-            >
-              {data?.url}
-            </td>
-            <td
-              style={{
-                fontSize: "13px",
-              }}
-            >
-              {data?.responseTime}
-            </td>
-            <td
-              style={{
-                fontSize: "13px",
-              }}
-            >
-              {moment(data?.timeInGST).format("DD-MMM-YYYY")}
-            </td>
-            <td
-              style={{
-                fontSize: "13px",
-              }}
-            >
-              {data?.userIp}
-            </td>
-            <td
-              style={{
-                fontSize: "13px",
-              }}
-            >
-              {moment(data?.createdAt).format("DD-MMM-YYYY")}
-            </td>
-
+      logsData.map((data, index) => (
+        <tr key={index}>
+          <td style={{ fontSize: "13px" }}>{data?.userName}</td>
+          <td style={{ fontSize: "13px" }}>{data?.method}</td>
+          <td style={{ fontSize: "13px", width: "10px" }}>{data?.url}</td>
+          <td style={{ fontSize: "13px" }}>{data?.responseTime}</td>
+          <td style={{ fontSize: "13px" }}>
+            {moment(data?.timeInGST).format("DD-MMM-YYYY")}
+          </td>
+          <td style={{ fontSize: "13px" }}>{data?.userIp}</td>
+          <td style={{ fontSize: "13px" }}>
+            {moment(data?.createdAt).format("DD-MMM-YYYY")}
+          </td>
+          <td>
             <span
               className={
                 data?.status >= 200 && data?.status <= 299
@@ -89,28 +51,47 @@ const UsersLogs = () => {
                   : "border border-danger fw-light badge badge-pill bg-danger"
               }
             >
-              <td>{data?.status}</td>
+              {data?.status}
             </span>
-          </tr>
-        ))}
-      </>
+          </td>
+        </tr>
+      ))
     );
   }, [logsData]);
+
   const fetchLogs = () => {
+    const pageNumber = currentPage + 1; 
     const data = {
-      apiEndpoint: `/admin/logs?page=${1}&size=${10}`,
+      apiEndpoint: `/admin/logs?page=${pageNumber}&size=${PER_PAGE_SIZE}`,
     };
     dispatch(userLogs(data)).then((res) => {
       if (res.type === "userLogs/fulfilled") {
         setLogsData(res?.payload?.data?.logs);
+        setPageCount(res?.payload?.data?.totalLogs / PER_PAGE_SIZE);
       }
     });
+  };
+
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected;
+    setCurrentPage(selectedPage);
   };
 
   return (
     <>
       <HeadingText className={"text-white display-5"} Text={"Users Logs"} />
       <UserTable headerData={tableHeadingArr} bodyData={tableData} />
+
+      {logsData.length > 0 && (
+        <ReactPaginate
+          pageCount={Math.ceil(pageCount)}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={3}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+        />
+      )}
     </>
   );
 };
